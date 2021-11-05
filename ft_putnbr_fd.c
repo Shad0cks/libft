395d221f1f7170ea57e0f1801bb2221ce040c367
Pierre-Louis <pierrelouisdeshayes42@gmail.com>
2021-11-05 11:57:46 +0100

    push 2/2 without bonus

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,17 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pdeshaye <pdeshaye@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: pdeshaye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 06:45:29 by pdeshaye          #+#    #+#             */
-/*   Updated: 2021/11/04 06:45:29 by pdeshaye         ###   ########.fr       */
+/*   Updated: 2021/11/05 11:40:57 by pdeshaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putchar(char c, int fd)
 {
-    (void)n;
-    (void)fd;
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int nb, int fd)
+{
+	if (nb == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-', fd);
+		ft_putnbr_fd(-nb, fd);
+		return ;
+	}
+	if (nb <= 9)
+	{
+		ft_putchar(nb + 48, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,9 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdeshaye <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 11:20:47 by pdeshaye          #+#    #+#             */
+/*   Updated: 2021/11/05 11:45:47 by pdeshaye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-    (void)s1;
-    (void)s2;
-    return (NULL);
+	char	*result;
+	int		size;
+
+	if (!s1 || !s2)
+		return (NULL);
+	size = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	result = malloc(size + 1);
+	if (result == NULL)
+		return (NULL);
+	result[size] = '\0';
+	ft_memcpy(result, s1, ft_strlen((char *)s1));
+	ft_memcpy(result + ft_strlen((char *)s1), s2, ft_strlen((char *)s2));
+	return (result);
 }
