31efdedc2e6e292eb226ca1cd869fb05384f1dad
Pierre-Louis <pierrelouisdeshayes42@gmail.com>
2021-11-07 19:11:17 +0100

    update ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -12,27 +12,20 @@
 
 #include "libft.h"
 
-static char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 char	*ft_strdup(char *src)
 {
 	char	*cped;
+	int	i;
 
 	cped = malloc(sizeof(char) * (ft_strlen(src) + 1));
 	if (cped == NULL)
 		return (NULL);
-	ft_strcpy(cped, src);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		cped[i] = src[i];
+		i++;
+	}
+	cped[i] = '\0';
 	return (cped);
 }
